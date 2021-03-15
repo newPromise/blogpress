46d975aedcdb215b16701c9e027da70c6b83479b
newPromise <250621458@qq.com>
2021-03-15 16:38:37 +0800

    feat: api path  composition

--- controllers/index.js
+++ controllers/index.js
@@ -1,19 +1,21 @@
 // 业务模块
-const article = require("./article");
-const tag = require("./tag");
+const article = require('./article');
+const tag = require('./tag');
 
-const allControllers = [article, { tag }];
-console.log("article", article);
-// 路由与控制函数映射
-let controllerMap = allControllers.reduce((pev, cur) => {
-    console.log("cur", cur);
-    const temp = {};
-    const mainKey = Object.keys(cur)[0];
-    for (const key in cur[mainKey]) {
-        temp[`/${mainKey}/${key}`] = cur[mainKey][key];
-    }
-    pev = Object.assign(pev, temp);
-    return pev;
+const ApiMap = {
+  article,
+  tag
+};
+
+// 收集 api 进行组合
+const ApiPathMap = Object.keys(ApiMap).reduce((map, api) => {
+  const childPaths = Object.keys(ApiMap[api]);
+  childPaths.forEach(path => {
+    const fullApi = `${api}/${path}`;
+    map[fullApi] = ApiMap[api][path];
+  });
+  return map;
 }, {});
 
-exports.controllers = controllerMap;
+
+module.exports = { ApiPathMap, ApiMap };
