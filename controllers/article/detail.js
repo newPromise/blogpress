120fe89b4e79fcf320ebd229866544733414a819
newPromise <250621458@qq.com>
2021-03-16 11:29:57 +0800

    feat: add query article

--- controllers/article/detail.js
+++ controllers/article/detail.js
@@ -1,6 +1,12 @@
-// 获取文章详情
-function getDetail() {
-  return { name: 'znn' };
+const { DbMethods } = require('../../db/index');
+const bson = require('bson');
+const listCollection = new DbMethods('list');
+
+
+function getDetail(params) {
+  const { id } = params;
+  listCollection.setCollection();
+  return listCollection.collection.findOne({ _id: new bson.ObjectId(id) });
 }
 
 module.exports = getDetail;
