9e91565ba4a5c75ed063e543f6dcb1628af628d7
newPromise <250621458@qq.com>
2021-03-24 11:20:25 +0800

    feat: add description

--- generater/parseMd.js
+++ generater/parseMd.js
@@ -1,10 +1,22 @@
 const fs = require('fs');
 const marked = require('marked');
 const yamlParse = require('js-yaml');
+const excludeDesc = [ 'space', 'code' ];
+const descLen = 77;
 
 function getPostInfo(text) {
   let yaml = yamlParse.load(text);
   return yaml;
+}
+
+// 获取发布文章的描述
+function getPostDesc(tokens) {
+  let desc = '';
+  Array.isArray(tokens) && tokens.filter(token => !excludeDesc.includes(token.type)).forEach(token => {
+    desc += token.text;
+    desc += getPostDesc(token.tokens);
+  });
+  return desc.slice(0, 77);
 }
 
 
@@ -15,6 +27,6 @@ function parse(mdPath) {
   const mdInfos = infoLexer[1].tokens[0];
   const postInfo = getPostInfo(mdInfos.raw);
   const postContent = marked.parser(lexerData);
-  return Object.assign(postInfo, { content: postContent });
+  return Object.assign(postInfo, { content: postContent, description: getPostDesc(lexerData) + '....' });
 }
 module.exports = parse;
